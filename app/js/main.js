a287fa4d957beafbd5aa5dc2c494a706baecd52a
TatishaS <snow_white_girl@bk.ru>
2023-03-02 01:31:17 +0300

    Footer & adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,54 @@
 'use strict';
 
+/* Burger menu */
+function burgerMenu() {
+  const burger = document.querySelector('.menu__burger');
+  const menuList = document.querySelector('.menu__list');
+
+  console.log(burger);
+
+  const overlay = document.querySelector('.overlay');
+  //const body = document.querySelector('body');
+  const menuLinks = document.querySelectorAll('.menu__list-itemlink');
+
+  function hideBurgerMenu() {
+    menuList.classList.remove('active');
+
+    burger.classList.remove('active-burger');
+    overlay.classList.remove('overlay--show');
+  }
+
+  burger.addEventListener('click', () => {
+    if (!menuList.classList.contains('active')) {
+      menuList.classList.add('active');
+      console.log('клик');
+
+      burger.classList.add('active-burger');
+
+      overlay.classList.add('overlay--show');
+    } else {
+      hideBurgerMenu();
+    }
+  });
+  /* Cкрываем мобильное меню по нажатию на ссылку в меню */
+  menuLinks.forEach(link => {
+    link.addEventListener('click', hideBurgerMenu);
+  });
+
+  // Брейкпойнт, на котором появляется бургер-меню
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 740) {
+      menuList.classList.remove('active');
+
+      burger.classList.remove('active-burger');
+    }
+  });
+
+  overlay.addEventListener('click', hideBurgerMenu);
+}
+
+burgerMenu();
+
 let swiper = new Swiper('.quality__swiper-slider', {
   slidesPerView: 1,
   autoplay: {
@@ -24,15 +73,4 @@ let swiper = new Swiper('.quality__swiper-slider', {
       return '<span class="' + currentClass + '"></span>';
     },
   },
-
-  /*   breakpoints: {
-    860: {
-      slidesPerView: 3,
-      spaceBetween: 22,
-    },
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-  }, */
 });
